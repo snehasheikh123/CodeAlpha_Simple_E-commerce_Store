9b2b316c7e0a712b59fd09de0ec5b4571f9c2344
snehasheikh123 <snehaazad123@gmail.com>
2025-04-30 19:34:40 +0530

    commit

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ $result = $conn->query($sql);  // Execute the query
 				            <h1 class="mb-4 mt-3">Shoes Collection 2019</h1>
 				            <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
 				            
-				            <p><a href="#" class="btn-custom">Discover Now</a></p>
+				            <p><a href="about.php" class="btn-custom">Discover Now</a></p>
 				          </div>
 		            </div>
 		          </div>
